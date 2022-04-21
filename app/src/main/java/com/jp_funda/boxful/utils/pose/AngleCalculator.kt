9126ed90a6a54c1b69ec10f5d881aea17156e2e8
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-22 08:29:11 +0900

    Add get angle function for joint which uses pointF

--- app/src/main/java/com/jp_funda/boxful/utils/pose/AngleCalculator.kt
+++ app/src/main/java/com/jp_funda/boxful/utils/pose/AngleCalculator.kt
@@ -1,5 +1,6 @@
 package com.jp_funda.boxful.utils.pose
 
+import android.graphics.PointF
 import com.google.mlkit.vision.pose.PoseLandmark
 import kotlin.math.abs
 import kotlin.math.atan2
@@ -12,7 +13,7 @@ object AngleCalculator {
     fun getAngle(
         firstPoint: PoseLandmark,
         midPoint: PoseLandmark,
-        lastPoint: PoseLandmark
+        lastPoint: PoseLandmark,
     ): Float {
         var result = Math.toDegrees(
             (atan2(
@@ -23,6 +24,32 @@ object AngleCalculator {
                 firstPoint.position.x - midPoint.position.x
             )).toDouble()
         )
+        result = abs(result) // Angle should never be negative
+        if (result > 180) {
+            result = 360.0 - result // Always get the acute representation of the angle
+        }
+        return result.toFloat()
+    }
+
+    /**
+     * Get angle for joint.
+     * @return value of calculated angle which is in range of 0 ~ 190 degree
+     */
+    fun getAngle(
+        firstPoint: PoseLandmark,
+        midPoint: PoseLandmark,
+        lastPoint: PointF,
+    ): Float {
+        var result = Math.toDegrees(
+            ((atan2(
+                lastPoint.y - midPoint.position.y,
+                lastPoint.x - midPoint.position.x
+            )) - atan2(
+                firstPoint.position.y - midPoint.position.y,
+                firstPoint.position.x - midPoint.position.x
+            )).toDouble()
+        )
+
         result = abs(result) // Angle should never be negative
         if (result > 180) {
             result = 360.0 - result // Always get the acute representation of the angle
