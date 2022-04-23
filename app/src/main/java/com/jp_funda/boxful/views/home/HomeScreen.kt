37a485ca5da61eb0bed4f5c122fa0b387098f1f1
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-23 19:50:53 +0900

    Add top section

--- app/src/main/java/com/jp_funda/boxful/views/home/HomeScreen.kt
+++ app/src/main/java/com/jp_funda/boxful/views/home/HomeScreen.kt
@@ -1,16 +1,18 @@
 package com.jp_funda.boxful.views.home
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -31,6 +33,12 @@ fun HomeScreen() {
                                 append("ful")
                             }
                         }
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    Text(
+                        text = stringResource(id = R.string.kickboxing),
+                        style = MaterialTheme.typography.caption,
+                        fontFamily = FontFamily.Serif,
                     )
                 },
                 navigationIcon = {
@@ -63,7 +71,6 @@ fun HomeMainContent(modifier: Modifier = Modifier) {
     val viewModel: HomeViewModel = hiltViewModel()
 
     Column(modifier = modifier) {
-        Text(viewModel.title)
         // Top Section
         TopSection()
 
@@ -78,19 +85,51 @@ fun HomeMainContent(modifier: Modifier = Modifier) {
 /** TopSection. */
 @Composable
 fun TopSection() {
-    Text(text = "Top")
+    // TODO add brand thumbnail image
+    Column {
+        Box(
+            modifier = Modifier
+                .background(Yellow500)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_service_thumbnail),
+                contentDescription = "Service thumbnail",
+            )
+        }
+        Text(
+            text = stringResource(id = R.string.home_service_slogan),
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp, end = 20.dp),
+            style = MaterialTheme.typography.h5,
+            fontFamily = FontFamily.Serif,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = stringResource(id = R.string.home_service_description),
+            modifier = Modifier.padding(start = 20.dp, top = 10.dp, end = 20.dp),
+            fontFamily = FontFamily.Serif
+        )
+    }
 }
 
 /** Dashboard Section for logged in user.  */
 @Composable
 fun DashboardSection() {
-    Text(text = "Dashboard")
+    // TODO add something to show
+    Text(
+        text = "Dashboard",
+        modifier = Modifier.padding(start = 20.dp),
+    )
 }
 
 @Composable
 fun MenuListSection() {
     Column {
-        Text(text = stringResource(id = R.string.home_menu_list))
+        Text(
+            text = stringResource(id = R.string.home_menu_list),
+            modifier = Modifier.padding(start = 20.dp),
+        )
         Row {
             // TODO menu card
         }
