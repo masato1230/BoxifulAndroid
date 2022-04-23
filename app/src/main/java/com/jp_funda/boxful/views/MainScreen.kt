be1c4c411128eb4e017605bb0043350c519c07fc
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-23 12:25:08 +0900

    Set up bottom navigation colors

--- app/src/main/java/com/jp_funda/boxful/views/MainScreen.kt
+++ app/src/main/java/com/jp_funda/boxful/views/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
@@ -12,6 +13,7 @@ import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.jp_funda.boxful.navigation.BottomBarMenuItem
 import com.jp_funda.boxful.navigation.BottomNavGraph
+import com.jp_funda.boxful.ui.theme.Yellow500
 
 @ExperimentalPermissionsApi
 @Composable
@@ -55,5 +57,7 @@ fun RowScope.AddItem(
         icon = { Icon(imageVector = menuItem.icon, contentDescription = "Navigation Icon") },
         selected = currentDestination?.hierarchy?.any { it.route == menuItem.route } == true,
         onClick = { navController.navigate(menuItem.route) },
+        selectedContentColor = Yellow500,
+        unselectedContentColor = Color.Gray,
     )
 }
