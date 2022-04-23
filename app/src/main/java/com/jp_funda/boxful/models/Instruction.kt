40502a7e09f4dbf05325b050d68cf7d3d5a6e1bf
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-24 08:35:14 +0900

    Add stretch cheking helper

--- app/src/main/java/com/jp_funda/boxful/models/Instruction.kt
+++ app/src/main/java/com/jp_funda/boxful/models/Instruction.kt
@@ -14,11 +14,15 @@ import com.jp_funda.boxful.utils.pose.AngleCalculator
 enum class Instruction(
     @StringRes
     val titleRes: Int,
-    val detectStartCallback: (allPoseLandmark: List<PoseLandmark>) -> Boolean,
-    val detectEndCallback: (allPoseLandmark: List<PoseLandmark>) -> Boolean,
+    val detectStartCallback: (pose: Pose) -> Boolean,
+    val detectEndCallback: (pose: Pose) -> Boolean,
 ) {
     LeftArmLeftPunch(
-        titleRes = stringResource(id = R.string.settings);
+        titleRes = R.string.instructions_left_hand_left_punch,
+        detectStartCallback = { checkIsLeftArmIsReadyToPunch(it) },
+        detectEndCallback = { checkIs }
+    )
+    ;
 
 
     companion object {
@@ -29,7 +33,7 @@ enum class Instruction(
          */
         private fun checkIsSatisfyMinimumInFrameLikelyFood(
             pose: Pose,
-            checkLandmarkIds: List<Int>,
+            checkLandmarkIds: Set<Int>,
             minimumLikelyFood: Float = 0.6f,
         ): Boolean {
             return checkLandmarkIds.all { id ->
@@ -39,14 +43,14 @@ enum class Instruction(
         }
 
         /**
-         * Check left arm is ready to punch.
+         * Check if the left arm is Folded.
          * When left arm is folded, then it is ready to punch
          */
-        fun checkIsLeftArmIsReadyToPunch(pose: Pose): Boolean {
+        fun checkLeftArmIsFolded(pose: Pose): Boolean {
             val isSatisfyMinimumInFlameLikelyFood =
                 checkIsSatisfyMinimumInFrameLikelyFood(
                     pose = pose,
-                    checkLandmarkIds = listOf(
+                    checkLandmarkIds = setOf(
                         PoseLandmark.LEFT_WRIST,
                         PoseLandmark.LEFT_ELBOW,
                         PoseLandmark.LEFT_SHOULDER,
@@ -65,14 +69,14 @@ enum class Instruction(
         }
 
         /**
-         * Check right arm is ready to punch.
+         * Check if the right arm is Folded.
          * When right arm is folded, then it is ready to punch
          */
-        fun checkIsRightArmIsReadyToPunch(pose: Pose): Boolean {
+        fun checkIsRightArmIsFolded(pose: Pose): Boolean {
             val isSatisfyMinimumInFlameLikelyFood =
                 checkIsSatisfyMinimumInFrameLikelyFood(
                     pose = pose,
-                    checkLandmarkIds = listOf(
+                    checkLandmarkIds = setOf(
                         PoseLandmark.RIGHT_WRIST,
                         PoseLandmark.RIGHT_ELBOW,
                         PoseLandmark.RIGHT_SHOULDER,
@@ -91,14 +95,14 @@ enum class Instruction(
         }
 
         /**
-         * Check left leg is ready to kick.
+         * Check if the left leg is lowered.
          * When left leg is lowered, then it is ready to kick
          */
-        fun checkIsLeftLegIsReadyToKick(pose: Pose): Boolean {
+        fun checkIsLeftLegIsLowered(pose: Pose): Boolean {
             val isSatisfyMinimumInFlameLikelyFood =
                 checkIsSatisfyMinimumInFrameLikelyFood(
                     pose = pose,
-                    checkLandmarkIds = listOf(
+                    checkLandmarkIds = setOf(
                         PoseLandmark.LEFT_KNEE,
                         PoseLandmark.LEFT_HIP,
                     )
@@ -116,14 +120,14 @@ enum class Instruction(
         }
 
         /**
-         * Check right leg is ready to kick.
+         * Check if the right leg is lowered.
          * When right leg is lowered, then it is ready to kick
          */
-        fun checkIsRightLegIsReadyToKick(pose: Pose): Boolean {
+        fun checkIsRightLegIsLowered(pose: Pose): Boolean {
             val isSatisfyMinimumInFlameLikelyFood =
                 checkIsSatisfyMinimumInFrameLikelyFood(
                     pose = pose,
-                    checkLandmarkIds = listOf(
+                    checkLandmarkIds = setOf(
                         PoseLandmark.RIGHT_KNEE,
                         PoseLandmark.RIGHT_HIP,
                     )
@@ -139,5 +143,105 @@ enum class Instruction(
                 rightLegAngle < 30
             }
         }
+
+        /**
+         * Check if the left arm is stretched.
+         */
+        fun checkIsLeftArmIsStretched(pose: Pose): Boolean {
+            val isSatisfyMinimumInFlameLikelyFood =
+                checkIsSatisfyMinimumInFrameLikelyFood(
+                    pose = pose,
+                    checkLandmarkIds = setOf(
+                        PoseLandmark.LEFT_WRIST,
+                        PoseLandmark.LEFT_ELBOW,
+                        PoseLandmark.LEFT_SHOULDER,
+                    )
+                )
+            return if (!isSatisfyMinimumInFlameLikelyFood) {
+                false
+            } else {
+                val leftArmAngle = AngleCalculator.getAngle(
+                    firstPoint = pose.getPoseLandmark(PoseLandmark.LEFT_WRIST)!!,
+                    midPoint = pose.getPoseLandmark(PoseLandmark.LEFT_ELBOW)!!,
+                    lastPoint = pose.getPoseLandmark(PoseLandmark.LEFT_SHOULDER)!!,
+                )
+                return leftArmAngle >= 130
+            }
+        }
+
+        /**
+         * Check if the right arm is stretched.
+         */
+        fun checkIsRightArmIsStretched(pose: Pose): Boolean {
+            val isSatisfyMinimumInFrameLikelyFood =
+                checkIsSatisfyMinimumInFrameLikelyFood(
+                    pose = pose,
+                    checkLandmarkIds = setOf(
+                        PoseLandmark.RIGHT_WRIST,
+                        PoseLandmark.RIGHT_ELBOW,
+                        PoseLandmark.RIGHT_SHOULDER,
+                    )
+                )
+            return if (!isSatisfyMinimumInFrameLikelyFood) {
+                false
+            } else {
+                val rightArmAngle = AngleCalculator.getAngle(
+                    firstPoint = pose.getPoseLandmark(PoseLandmark.RIGHT_WRIST)!!,
+                    midPoint = pose.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)!!,
+                    lastPoint = pose.getPoseLandmark(PoseLandmark.RIGHT_SHOULDER)!!,
+                )
+                return rightArmAngle >= 130
+            }
+        }
+
+        /**
+         * Check if the left leg is stretched.
+         */
+        fun checkIsLeftLegIsStretched(pose: Pose): Boolean {
+            val isSatisfyMinimumInFrameLikelyFood =
+                checkIsSatisfyMinimumInFrameLikelyFood(
+                    pose = pose,
+                    checkLandmarkIds = setOf(
+                        PoseLandmark.LEFT_KNEE,
+                        PoseLandmark.LEFT_HIP,
+                    )
+                )
+            return if (!isSatisfyMinimumInFrameLikelyFood) {
+                false
+            } else {
+                val leftLegAngle = AngleCalculator.getAngle(
+                    firstPoint = pose.getPoseLandmark(PoseLandmark.LEFT_KNEE)!!,
+                    midPoint = pose.getPoseLandmark(PoseLandmark.LEFT_HIP)!!,
+                    lastPoint = PointF(0f, Float.MAX_VALUE),
+                )
+                return leftLegAngle > 60
+            }
+        }
+
+        /**
+         * Check if the right leg is stretched.
+         */
+        fun checkIsRightLegIsStretched(pose: Pose): Boolean {
+            val isSatisfyMinimumInFrameLikelyFood =
+                checkIsSatisfyMinimumInFrameLikelyFood(
+                    pose = pose,
+                    checkLandmarkIds = setOf(
+                        PoseLandmark.LEFT_KNEE,
+                        PoseLandmark.LEFT_HIP,
+                    )
+                )
+            return if (!isSatisfyMinimumInFrameLikelyFood) {
+                false
+            } else {
+                val rightLegAngle = AngleCalculator.getAngle(
+                    firstPoint = pose.getPoseLandmark(PoseLandmark.RIGHT_KNEE)!!,
+                    midPoint = pose.getPoseLandmark(PoseLandmark.RIGHT_HIP)!!,
+                    lastPoint = PointF(0f, Float.MAX_VALUE),
+                )
+                return rightLegAngle > 60
+            }
+        }
+
+        // TODO left leg stretch
     }
 }
