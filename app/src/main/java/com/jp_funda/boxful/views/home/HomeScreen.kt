a1884a087da6950845d305f2bf1146ddba5235aa
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-23 18:23:52 +0900

    Add empty sections for home

--- app/src/main/java/com/jp_funda/boxful/views/home/HomeScreen.kt
+++ app/src/main/java/com/jp_funda/boxful/views/home/HomeScreen.kt
@@ -1,10 +1,14 @@
 package com.jp_funda.boxful.views.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
@@ -43,17 +47,52 @@ fun HomeScreen() {
             )
         }
     ) {
-        it
-        HomeMainContent()
+        HomeMainContent(modifier = Modifier.padding(it))
     }
 }
 
 /**
- * ホーム画面メイン.
- *
+ * HomeMainContent.
+ * This contains
+ * ・Top Section
+ * ・Dashboard Section for logged in user
+ * ・Menu List Section
  */
 @Composable
-fun HomeMainContent() {
+fun HomeMainContent(modifier: Modifier = Modifier) {
     val viewModel: HomeViewModel = hiltViewModel()
-    Text(viewModel.title)
+
+    Column(modifier = modifier) {
+        Text(viewModel.title)
+        // Top Section
+        TopSection()
+
+        // Dashboard Section for logged in user
+        DashboardSection()
+
+        // Menus List Section for logged in users
+        MenuListSection()
+    }
+}
+
+/** TopSection. */
+@Composable
+fun TopSection() {
+    Text(text = "Top")
+}
+
+/** Dashboard Section for logged in user.  */
+@Composable
+fun DashboardSection() {
+    Text(text = "Dashboard")
+}
+
+@Composable
+fun MenuListSection() {
+    Column {
+        Text(text = stringResource(id = R.string.home_menu_list))
+        Row {
+            // TODO menu card
+        }
+    }
 }
