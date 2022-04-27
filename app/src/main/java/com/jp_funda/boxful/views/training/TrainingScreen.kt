2b738b54fe28e8873eedd4d15196cf7651e1e30c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-28 07:47:08 +0900

    Use original dialog llayout instead of permission request result

--- app/src/main/java/com/jp_funda/boxful/views/training/TrainingScreen.kt
+++ app/src/main/java/com/jp_funda/boxful/views/training/TrainingScreen.kt
@@ -1,25 +1,15 @@
 package com.jp_funda.boxful.views.training
 
-import android.content.Intent
-import android.net.Uri
-import android.provider.Settings
 import android.util.Log
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.jp_funda.boxful.models.SingleMenu
-import com.jp_funda.boxful.views.components.Permission
+import com.jp_funda.boxful.views.components.RequestCameraPermission
 import com.jp_funda.boxful.views.components.pose.CameraPreview
 import com.jp_funda.boxful.views.components.pose.PoseGraphic
 
@@ -43,22 +33,8 @@ fun TrainingMainContent(modifier: Modifier = Modifier, navController: NavControl
     val viewModel = hiltViewModel<TrainingViewModel>()
     Log.d("Single Menu", viewModel.getSingleMenu().name)
 
-    val context = LocalContext.current
-    Permission(
-        permission = android.Manifest.permission.CAMERA,
-        permissionNotAvailableContent = {
-            Column(modifier) {
-                Text("O noes! No Camera!")
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(onClick = {
-                    context.startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-                        data = Uri.fromParts("package", context.packageName, null)
-                    })
-                }) {
-                    Text("Open Settings")
-                }
-            }
-        }
+    RequestCameraPermission(
+        navController = navController,
     ) {
         CameraPreview()
         PoseGraphic()
