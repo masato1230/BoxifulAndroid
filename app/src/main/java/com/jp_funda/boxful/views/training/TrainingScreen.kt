830b6c0aeb03363f701de21f2d0d87a396b14148
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-28 08:56:03 +0900

    Add instruction overlauy base

--- app/src/main/java/com/jp_funda/boxful/views/training/TrainingScreen.kt
+++ app/src/main/java/com/jp_funda/boxful/views/training/TrainingScreen.kt
@@ -1,19 +1,26 @@
 package com.jp_funda.boxful.views.training
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.jp_funda.boxful.models.Instruction
 import com.jp_funda.boxful.models.SingleMenu
+import com.jp_funda.boxful.ui.theme.BlackAlpha50
 import com.jp_funda.boxful.views.components.RequestCameraPermission
 import com.jp_funda.boxful.views.components.pose.CameraPreview
 import com.jp_funda.boxful.views.components.pose.PoseGraphic
@@ -57,7 +64,18 @@ fun TrainingMainContent(modifier: Modifier = Modifier, navController: NavControl
  */
 @Composable
 fun InstructionOverlay(instruction: Instruction) {
-    Column {
-        Text(text = stringResource(id = instruction.titleRes))
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(BlackAlpha50)
+            .padding(vertical = 30.dp)
+    ) {
+        Text(
+            text = stringResource(id = instruction.titleRes),
+            fontSize = MaterialTheme.typography.h4.fontSize * 1.2,
+            fontWeight = FontWeight.ExtraBold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
