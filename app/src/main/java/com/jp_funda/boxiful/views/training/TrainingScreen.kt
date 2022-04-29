199e8b840e057d86b57aa08503b9ad8bb6e68587
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-29 10:21:18 +0900

    Add icon color and rotate state

--- app/src/main/java/com/jp_funda/boxiful/views/training/TrainingScreen.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/training/TrainingScreen.kt
@@ -1,35 +1,23 @@
 package com.jp_funda.boxiful.views.training
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.jp_funda.boxiful.R
 import com.jp_funda.boxiful.models.Instruction
 import com.jp_funda.boxiful.models.SingleMenu
-import com.jp_funda.boxiful.ui.theme.BlackAlpha50
 import com.jp_funda.boxiful.views.components.RequestCameraPermission
 import com.jp_funda.boxiful.views.components.pose.CameraPreview
 import com.jp_funda.boxiful.views.components.pose.PoseGraphic
+import com.jp_funda.boxiful.views.training.component.BottomInstructionOverlay
 
 @ExperimentalPermissionsApi
 @Composable
@@ -74,33 +62,9 @@ fun InstructionOverlay(instruction: Instruction) {
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Bottom,
     ) {
-//        Spacer(modifier = Modifier.weight(1f))
+        // TODO Upper
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(BlackAlpha50)
-                .padding(vertical = 10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = stringResource(id = instruction.titleRes),
-                fontSize = MaterialTheme.typography.h4.fontSize * 1.2,
-                fontWeight = FontWeight.ExtraBold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth(),
-            )
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Icon(
-                imageVector = Icons.Rounded.ArrowBack,
-                contentDescription = stringResource(id = R.string.desc_left),
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(CircleShape)
-                    .background(Color.Red)
-            )
-        }
+        // Bottom
+        BottomInstructionOverlay(instruction)
     }
 }
