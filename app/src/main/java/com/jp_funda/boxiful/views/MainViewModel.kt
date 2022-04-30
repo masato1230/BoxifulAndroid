3b0878e278efffdf8a1052f12a53c1a04de50889
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-30 09:12:28 +0900

    Add ScoreCalculator util for calculate single menu score

--- app/src/main/java/com/jp_funda/boxiful/views/MainViewModel.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/MainViewModel.kt
@@ -6,6 +6,10 @@ import com.jp_funda.boxiful.models.SingleMenuScores
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+/**
+ * ViewModel for MainActivity.
+ * use this view model only for sharing large data between screens or concerns of activity.
+ */
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
     /** Single Menu scores */
