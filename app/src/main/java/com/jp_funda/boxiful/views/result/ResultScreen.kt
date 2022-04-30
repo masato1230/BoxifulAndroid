88a4a58deabff1f295b5827a4e218ae7cf9c6b8b
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-04-30 16:37:20 +0900

    Add navigation and di settings to result

--- app/src/main/java/com/jp_funda/boxiful/views/result/ResultScreen.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/result/ResultScreen.kt
@@ -1,10 +1,26 @@
 package com.jp_funda.boxiful.views.result
 
+import android.annotation.SuppressLint
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.jp_funda.boxiful.models.SingleMenuScores
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
-fun ResultScreen(singleMenuScores: SingleMenuScores) {
-    Text(text = singleMenuScores.singleMenu.name)
+fun ResultScreen(navController: NavController, singleMenuScores: SingleMenuScores) {
+    // Set result scores to viewModel
+    hiltViewModel<ResultViewModel>().setSingleMenuScores(singleMenuScores)
+
+    Scaffold {
+        ResultMainContent(navController = navController)
+    }
+}
+
+@Composable
+fun ResultMainContent(navController: NavController) {
+    val viewModel = hiltViewModel<ResultViewModel>()
+    Text(text = "Hello")
 }
