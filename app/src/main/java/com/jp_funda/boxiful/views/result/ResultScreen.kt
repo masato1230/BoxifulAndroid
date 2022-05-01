3dcc6b1e909bf9a905e3eed56191563f8e8c37bb
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-01 09:18:13 +0900

    Add all over score

--- app/src/main/java/com/jp_funda/boxiful/views/result/ResultScreen.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/result/ResultScreen.kt
@@ -1,11 +1,24 @@
 package com.jp_funda.boxiful.views.result
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jp_funda.boxiful.models.SingleMenuScores
+import com.jp_funda.boxiful.ui.theme.Red500
 import com.jp_funda.boxiful.views.components.AnimatedPieChart
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
@@ -22,5 +35,26 @@ fun ResultScreen(navController: NavController, singleMenuScores: SingleMenuScore
 @Composable
 fun ResultMainContent(navController: NavController) {
     val viewModel = hiltViewModel<ResultViewModel>()
-    AnimatedPieChart(indicatorValue = 50f)
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "トレーニング評価",
+            color = Color.White,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.ExtraBold,
+            fontFamily = FontFamily.Serif,
+            modifier = Modifier.padding(10.dp)
+        )
+        Box(
+            modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+            contentAlignment = Alignment.Center,
+        ) {
+            AnimatedPieChart(
+                indicatorValue = 88f,
+                indicatorColor = Red500,
+                modifier = Modifier.fillMaxSize(),
+            )
+            Text(text = "88点", color = Color.LightGray, style = MaterialTheme.typography.h3)
+        }
+    }
 }
