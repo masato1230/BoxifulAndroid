9a7c06d41eed41dc178e1a0aa90553b672d521e3
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-03 17:39:10 +0900

    Add AuthPreferences and di definitions

--- app/src/main/java/com/jp_funda/boxiful/di/AppModule.kt
+++ app/src/main/java/com/jp_funda/boxiful/di/AppModule.kt
@@ -1,9 +1,12 @@
 package com.jp_funda.boxiful.di
 
+import android.content.Context
 import com.jp_funda.boxiful.AppUtils
+import com.jp_funda.boxiful.data.shared_preference.AuthPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -13,4 +16,9 @@ class AppModule {
     @Provides
     @Singleton
     fun provideAppUtils() = AppUtils()
+
+    @Provides
+    @Singleton
+    fun provideAuthPreferences(@ApplicationContext appContext: Context) =
+        AuthPreferences(appContext)
 }
