47463725e33bd8cca30c032a6a734101a959fe1c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-03 11:35:10 +0900

    Add base components for login

--- app/src/main/java/com/jp_funda/boxiful/views/login/LoginScreen.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/login/LoginScreen.kt
@@ -1,17 +1,24 @@
 package com.jp_funda.boxiful.views.login
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.jp_funda.boxiful.R
+import com.jp_funda.boxiful.ui.theme.BlackAlpha80
+import com.jp_funda.boxiful.ui.theme.Yellow500
 import com.jp_funda.boxiful.views.components.header.Header
 
 @Composable
@@ -29,7 +36,46 @@ fun LoginMainContent(modifier: Modifier = Modifier, navController: NavController
     Image(
         painter = painterResource(id = R.drawable.bg_login),
         contentScale = ContentScale.FillHeight,
-        contentDescription = "",
+        contentDescription = "", // TODO
         modifier = Modifier.fillMaxSize(),
     )
+
+    // Foreground contents
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 50.dp, vertical = 100.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(10.dp))
+                .background(BlackAlpha80)
+        ) {
+            Column(modifier = Modifier.padding(20.dp)) {
+                Text(
+                    text = stringResource(id = R.string.login_en),
+                    color = Color.White,
+                    style = MaterialTheme.typography.h5,
+                    fontWeight = FontWeight.ExtraBold,
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                TextField(value = "", onValueChange = {}, placeholder = { Text(text = "メールアドレス") })
+                Spacer(modifier = Modifier.height(10.dp))
+                TextField(value = "", onValueChange = {}, placeholder = { Text(text = "パスワード") })
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Yellow500),
+                    ) {
+                        Text(text = stringResource(id = R.string.login))
+                    }
+                }
+            }
+        }
+    }
 }
