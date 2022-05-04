5e78c80ea3e4f1854ca1039a697ade4729ec526c
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-05 08:33:35 +0900

    Add week iteration for heat map

--- app/src/main/java/com/jp_funda/boxiful/views/components/calendar_heat_map/CalendarHeatMap.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/components/calendar_heat_map/CalendarHeatMap.kt
@@ -1,9 +1,12 @@
 package com.jp_funda.boxiful.views.components.calendar_heat_map
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import com.jp_funda.boxiful.utils.date.DateIterator
+import java.time.DayOfWeek
 import java.time.LocalDate
 
 @Composable
@@ -12,6 +15,26 @@ fun CalendarHeatMap(
     endDate: LocalDate = LocalDate.now()
 ) {
     val dates = DateIterator(startDate, endDate, 1)
+    val datesSeparatedByDate = mutableListOf<List<LocalDate>>()
+
+    var datesInOneWeek = mutableListOf<LocalDate>()
+    dates.forEach { date ->
+        datesInOneWeek.add(date)
+        if (date.dayOfWeek == DayOfWeek.SATURDAY || date == endDate) {
+            datesSeparatedByDate.add(datesInOneWeek)
+            datesInOneWeek = mutableListOf()
+        }
+    }
+
+    LazyRow() {
+        items(datesSeparatedByDate) { datesInOneColumn ->
+            Column() {
+                datesInOneColumn.forEach { date ->
+                    Text(text = date.toString())
+                }
+            }
+        }
+    }
 
     Column() {
         dates.forEach { date ->
