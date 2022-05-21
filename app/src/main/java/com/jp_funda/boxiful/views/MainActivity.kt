238a3c1f12fe30cd9f620972bc106eca9a7c55ed
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-21 09:31:48 +0900

    Remove extra scaffold

--- app/src/main/java/com/jp_funda/boxiful/views/MainActivity.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.jp_funda.boxiful.views
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,14 +8,16 @@ import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.android.gms.ads.*
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.jp_funda.boxiful.BuildConfig
@@ -31,33 +32,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<MainViewModel>()
     var interstitialAd: InterstitialAd? = null
+    private lateinit var adRequest: AdRequest
 
-    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
-        // Load Interstitial Ad
-        // Initialize Mobile Ads SDK
-        MobileAds.initialize(this)
-        // Specify test device
-        MobileAds.setRequestConfiguration(
-            RequestConfiguration.Builder()
-                .setTestDeviceIds(listOf("2680A52322369B561BAF8E9FAEC62987"))
-                .build()
-        )
-        val adRequest = AdRequest.Builder().build()
-        InterstitialAd.load(this, BuildConfig.interstitialUnitId, adRequest,
-            object : InterstitialAdLoadCallback() {
-                override fun onAdFailedToLoad(p0: LoadAdError) {
-                    interstitialAd = null
-                }
-
-                override fun onAdLoaded(loadedAd: InterstitialAd) {
-                    interstitialAd = loadedAd
-                    interstitialAd?.show(this@MainActivity)
-                }
-            })
+        // AdMob initialization
+        initializeInterstitialAd()
 
         // Refresh auth tokens
         viewModel.refreshAuthTokens()
@@ -75,21 +57,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    Scaffold(
-                        bottomBar = {
-                            AndroidView(factory = {
-                                AdView(it).apply {
-                                    adSize = AdSize.BANNER
-                                    adUnitId = BuildConfig.interstitialUnitId
-                                    loadAd(adRequest)
-                                }
-                            })
-                        }
-                    ) {
-                        MainScreen(viewModel)
+                    LaunchedEffect(key1 = "") {
+                        showInterstitial()
                     }
+                    MainScreen(viewModel)
                 }
             }
+        }
+    }
+
+    private fun initializeInterstitialAd() {
+        // Load Interstitial Ad
+        // Initialize Mobile Ads SDK
+        MobileAds.initialize(this)
+        // Specify test device
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTestDeviceIds(listOf("2680A52322369B561BAF8E9FAEC62987"))
+                .build()
+        )
+        adRequest = AdRequest.Builder().build()
+        InterstitialAd.load(this, BuildConfig.interstitialUnitId, adRequest,
+            object : InterstitialAdLoadCallback() {
+                override fun onAdFailedToLoad(p0: LoadAdError) {
+                    interstitialAd = null
+                }
+
+                override fun onAdLoaded(loadedAd: InterstitialAd) {
+                    interstitialAd = loadedAd
+                    interstitialAd?.show(this@MainActivity)
+                }
+            })
+    }
+
+    fun showInterstitial() {
+        if (interstitialAd != null) {
+            interstitialAd?.show(this)
         }
     }
 }
