8897d66f9341d665e788acd059ff04e35cb84519
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-05-24 08:12:57 +0900

    Fix navigation bar color

--- app/src/main/java/com/jp_funda/boxiful/views/MainScreen.kt
+++ app/src/main/java/com/jp_funda/boxiful/views/MainScreen.kt
@@ -2,12 +2,14 @@ package com.jp_funda.boxiful.views
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination
@@ -19,6 +21,8 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.jp_funda.boxiful.navigation.BottomBarMenuItem
 import com.jp_funda.boxiful.navigation.BottomNavGraph
+import com.jp_funda.boxiful.navigation.NavigationRoutes
+import com.jp_funda.boxiful.ui.theme.Pink400
 import com.jp_funda.boxiful.ui.theme.Yellow500
 
 @ExperimentalPagerApi
@@ -75,8 +79,9 @@ fun RowScope.AddItem(
         label = { Text(text = stringResource(id = menuItem.titleRes)) },
         icon = { Icon(imageVector = menuItem.icon, contentDescription = "Navigation Icon") },
         selected = currentDestination?.hierarchy?.any { it.route == menuItem.route } == true,
-        onClick = { navController.navigate(menuItem.route) },
-        selectedContentColor = Yellow500,
-        unselectedContentColor = Color.Gray,
+        onClick = { navController.navigate(menuItem.route) { popUpTo(NavigationRoutes.HOME) } },
+        selectedContentColor = Pink400,
+        unselectedContentColor = Color.White,
+        modifier = Modifier.background(Yellow500)
     )
 }
